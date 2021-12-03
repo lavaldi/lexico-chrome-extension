c5450cdadcfd1b7156139e879ec31350624645f3
Claudia Valdivieso <claumavaldivieso@gmail.com>
2021-12-02 21:44:45 -0500

    format

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.omnibox.onInputEntered.addListener(function(text) {
+chrome.omnibox.onInputEntered.addListener(function (text) {
   var newURL =
     "https://www.lexico.com/en/definition/" + encodeURIComponent(text);
   chrome.tabs.update({ url: newURL });
